88bafabd5f2f7475e0fb216a950d514b59435dcf
清水稜也 <ryoya0111g.o.l.f@gmail.com>
2023-10-11 11:00:40 +0900

    next-js基礎学習

---
+++ pages/api/sayHello.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type HelloResponse = {
+    name: string
+}
+
+export default (request: NextApiRequest, response: NextApiResponse<HelloResponse>) => {
+    response.status(200).json({name: 'Ryo'})
+}
