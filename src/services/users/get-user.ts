d5544f96de6173ad28b1beacb430497256d3e87e
清水稜也 <ryoya0111g.o.l.f@gmail.com>
2023-10-22 23:00:22 +0900

    アプリケーション開発練習

---
+++ src/services/users/get-user.ts
@@ -0,0 +1,29 @@
+import { ApiContext, User } from 'types'
+import { fetcher } from '@/util'
+
+export type GetUserParams = {
+  id: number
+}
+
+/**
+ * ユーザーAPI (個別取得)
+ * @param context  APIコンテキスト
+ * @param params パラメータ
+ * @returns ユーザー
+ */
+const getUser = async (
+  context: ApiContext,
+  { id }: GetUserParams,
+): Promise<User> => {
+  return await fetcher(
+    `${context.apiRootUrl.replace(/\/$/g, '')}/users/${id}`,
+    {
+      headers: {
+        Accept: 'application/json',
+        ContentType: 'application/json',
+      },
+    },
+  )
+}
+
+export default getUser
